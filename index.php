fc1add8fc13048a9e7906d157a28d3d3939cd6cc
Bartosz <bartek.planiak@gmail.com>
2020-08-25 12:58:16 +0200

    Added necessary files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <?php require "templates/head.html"; ?>
+  <body>
+
+    <script src="index.js" charset="utf-8"></script>
+  </body>
+</html>
