cf29d7262de4c45caf2fa83312f2ad301bbc0c83
Bartek <bartek.planiak@gmail.com>
2020-10-03 21:34:20 +0200

    Started doing town-hall interface

---
+++ php/townhall.php
@@ -0,0 +1,14 @@
+<?php require("game.php"); ?>
+<div class="townhall">
+  <h2 class="townhall__title">Town hall</h2>
+  <div class="buildings_container">
+    <div class="building">
+      <div class="building__title">Sawmill</div>
+      <div class="building__costs">
+        <div class="building__cost"></div>
+        <div class="building__cost"></div>
+        <div class="building__cost"></div>
+      </div>
+    </div>
+  </div>  
+</div>
